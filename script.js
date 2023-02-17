0b10c357272e0f82424f2821209f001f326d7f94
joelstoppa <joel.stoppa@gmail.com>
2023-02-17 15:15:20 +0100

    Add function for each button which plays a round when button is pressed

--- script.js
+++ script.js
@@ -14,73 +14,85 @@ function getComputerChoice() {
 
 
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
     
+    let computerSelection = getComputerChoice()
+
     let win = `You win! ${playerSelection} beats ${computerSelection}!`;
     let lose = `Computer wins! ${computerSelection} beats ${playerSelection}!`;
     let tie = `It's a tie! You chose ${playerSelection} and computer chose ${computerSelection}`;
     
     if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissors' && computerSelection === 'paper')) {
         
+        console.log('win')
         return { result: win, outcome: 'win' };
     }
     else if (playerSelection === computerSelection) {
+        
+        console.log('tie')
         return { result: tie, outcome: 'tie' };
     }
     else {
+        
+        console.log('lose')
         return {result: lose, outcome: 'lose' };
     }
     
     
-    }
-
-
-
-function playGame() {
     
-    let playerScore = 0;
-    let computerScore = 0;
+    }
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener('click', () => {
+    playRound(button.id)
+}));
 
-    for (let round = 0; round < 5; round++) {
+
+
+// function playGame() {
+    
+//     let playerScore = 0;
+//     let computerScore = 0;
+
+//     for (let round = 0; round < 5; round++) {
         
-        let computerSelection = getComputerChoice();
-        let playerSelection = prompt('Rock paper or scissors?').toLowerCase();
-        let roundResult = playRound(playerSelection, computerSelection);
+//         let computerSelection = getComputerChoice();
+//         let playerSelection = prompt('Rock paper or scissors?').toLowerCase();
+//         let roundResult = playRound(playerSelection, computerSelection);
 
-        console.log(roundResult.result);
+//         console.log(roundResult.result);
 
-        if (roundResult.outcome === 'win') {
-            playerScore++;
-        }
-        else if (roundResult.outcome === 'lose') {
-            computerScore++;
-        }
+//         if (roundResult.outcome === 'win') {
+//             playerScore++;
+//         }
+//         else if (roundResult.outcome === 'lose') {
+//             computerScore++;
+//         }
 
-        console.log('Player score: ' + playerScore);
-        console.log('Computer score: ' + computerScore);
+//         console.log('Player score: ' + playerScore);
+//         console.log('Computer score: ' + computerScore);
 
-        if (playerScore >= 3 || computerScore >= 3) {
-            break;
-        }
+//         if (playerScore >= 3 || computerScore >= 3) {
+//             break;
+//         }
 
-    }
+//     }
 
-    if (playerScore > computerScore) {
-        console.log('You won the game!');
-    }
-    else if (playerScore < computerScore) {
-        console.log('You lost the game.');
-    }
-    else {
-        console.log('The game was a tie.');
-    }
+//     if (playerScore > computerScore) {
+//         console.log('You won the game!');
+//     }
+//     else if (playerScore < computerScore) {
+//         console.log('You lost the game.');
+//     }
+//     else {
+//         console.log('The game was a tie.');
+//     }
 
         
 
-    }
+//     }
 
 
 
 
 
-playGame();
+// playGame();
